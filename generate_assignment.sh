d48439d144311519028795edb2244980d9a5bac3
Bart De Vylder <bartdevylder@gmail.com>
2017-06-08 17:57:33 +0200

    serving from release directory (giving up on MyBinder

--- generate_assignment.sh
+++ generate_assignment.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 set -e
 nbgrader assign 1 --force
-cp release/1/index.ipynb .
-
-rm -r data
-mkdir data
-cp source/1/data/* data/
-
-rm -r images
-mkdir images
-cp source/1/images/* images/

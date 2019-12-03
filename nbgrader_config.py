abb8bde2cc7baa60de737c8e0931826f609c3a9b
jbo <jeroen.boeye@faction.xyz>
2019-12-03 09:46:33 +0100

    styling

--- nbgrader_config.py
+++ nbgrader_config.py
@@ -3,4 +3,4 @@ c.NbGrader.db_assignments = [dict(name="1", duedate="2017-01-01 17:00:00 UTC")]
 c.NbGrader.db_students = [
     dict(id="foo", first_name="foo", last_name="foo")
 ]
-c.ClearSolutions.code_stub = {'python' : '##### Implement this part of the code #####\nraise NotImplementedError()'}
+c.ClearSolutions.code_stub = {'python': '##### Implement this part of the code #####\nraise NotImplementedError()'}

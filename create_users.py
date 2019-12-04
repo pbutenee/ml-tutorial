02c340dc3553b1457f31bd3021968d5e8334f0e6
jeroenboeye <jeroenboeye@gmail.com>
2019-12-04 13:42:30 +0100

    Upgrade to python 3

--- create_users.py
+++ create_users.py
@@ -1,27 +1,27 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 import subprocess as p
 import os
 import sys
 
 if len(sys.argv) != 3:
-    print "Usage %s <number_of_users> <password>" % sys.argv[0]
+    print(f"Usage {sys.argv[0]} <number_of_users> <password>")
     sys.exit()
 
 N = int(sys.argv[1])
-PWD = sys.argv[2]
+PWD = str(sys.argv[2])
 
 if N > 1000:
-    raise Exception("Cannot create more than 1000 users (%d asked)" % N)
+    raise Exception(f"Cannot create more than 1000 users ({N} asked)")
 
 for ix in range(N):
     read, write = os.pipe()
-    os.write(write, '%s\n%s' % (PWD, PWD))
+    os.write(write, str.encode(f'{PWD}\n{PWD}'))
     os.close(write)
 
-    username = "user%03d" % ix
-    print "Adding user " + username
+    username = f"user{ix:03}"
+    print(f"Adding user {username}")
 
     p.check_call(['adduser', '--gecos', '""', username], stdin=read)
-    p.check_call(['cp', '-R', '/home/cs/tutorial', '/home/%s' % username])
-    p.check_call(['chown', '-R', '%s:%s' % (username, username), '/home/%s/tutorial' % username])
+    p.check_call(['cp', '-R', '/home/cs/tutorial', f'/home/{username}'])
+    p.check_call(['chown', '-R', f'{username}:{username}', f'/home/{username}/tutorial'])
